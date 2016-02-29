b1c6a2448282cca07f3b48d33f6e97df3224f458
Ben Alman <EMAIL>
2016-02-29 16:21:41 +0100

    working liquidprompt config copy

--- init/60_liquidprompt.sh
+++ init/60_liquidprompt.sh
@@ -1,3 +1,3 @@
 mkdir "$DOTFILES/tmp" && cd "$DOTFILES/tmp"
 git clone https://github.com/nojhan/liquidprompt.git
-cp "$DOTFILES/conf/liquidpromptrc" "~/.config/"
+cp "$DOTFILES/conf/liquidpromptrc" ~/.config/
